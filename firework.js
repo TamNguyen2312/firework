aef35aa02d40e731e573fe4016c3d5816bf7dc9f
TamNguyen2312 <th3m1nd05@gmail.com>
2025-04-30 23:07:28 +0700

    Update firework.js

--- firework.js
+++ firework.js
@@ -221,7 +221,7 @@ function createParticles(x, y) {
   explosionSound.play();
 
   // increase the particle count for a bigger explosion, beware of the canvas performance hit with the increased particles though
-  var particleCount = 150;
+  var particleCount = 100;
   while (particleCount--) {
     particles.push(new Particle(x, y));
   }
